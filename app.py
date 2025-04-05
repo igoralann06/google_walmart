ef3b74e91c2898bbddcfec79a1d6f211bfc8f5ed
igor <igoralann113@gmail.com>
2025-04-06 01:23:47 +0900

    updated

--- app.py
+++ app.py
@@ -326,8 +326,7 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0", 
         port=5000,
-        threaded=True,
-        request_timeout=None  # Disable request timeout
+        threaded=True
     )
 
 
